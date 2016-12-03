16dffe0e43824ae0e24b365c3ca202bcdf2bfa64
Mikhail163 <mephist0_87@mail.ru>
2016-12-03 14:05:34 +0700

    gulp nmp update

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,9 @@ var concatCss = require('gulp-concat-css');
 var cleanCSS  = require('gulp-clean-css');
 var rename    = require('gulp-rename');
  
- 
+
 gulp.task('default', function () {
-  return gulp.src(['css/*.css', 'bootstrap/css/bootstrap.min.css'])
+  return gulp.src(['css/*.css', 'node_modules/bootstrap/dist/css/bootstrap.css'])
     .pipe(concatCss("main.css"))
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(rename('main.min.css'))
